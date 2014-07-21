7dad50c1466b437cd6d5cb8e56b0721fb5f2f9f2
Matthew Falbo <matthew.falbo@gmail.com>
2014-07-21 12:04:06 -0600

    Testing different functionality and settings within the scene.

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,22 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+console.log(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry(2,2,2);
+var geometry = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+// var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+// scene.add( light );
+
+//Adjust camera position in the x,y, or z axis
+camera.position.z = 3;
+// camera.position.x = 5;
+// camera.position.y = -3;
+
 
 function render() {
 	requestAnimationFrame(render);
